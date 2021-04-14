6102e225edb36301916fe4b5171e536a8fcdf52b
Juli-Kumari <72196282+Juli-Kumari@users.noreply.github.com>
2021-04-14 20:45:37 +0530

    create banana2

--- app.js
+++ app.js
@@ -8,9 +8,19 @@
 // //output
 // alert(welcomeMessage)
 
-var btnTranslate=document.querySelector("#btn-translate")
+var btnTranslate=document.querySelector("#btn-translate");
 // console.log(btnTranslate)
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!")
+var txtInput=document.querySelector("#txt-input");
+console.log(txtInput)
+var outputDiv= document.querySelector("#output");
+// console.log(outputDiv)
+// outputDiv.innerText="juli kumari" // for write element
 
-})
+function clickHandler(){
+    // console.log("clicked!")
+    // console.log("input",txtInput.value) // read element   // taking input from console
+    outputDiv.innerText="ajsjsjsjsjs "+textInput.value;
+
+};
+btnTranslate.addEventListener("click",clickHandler)
+
