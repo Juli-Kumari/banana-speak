541ef12412fe9308dab3e18c3bee1a5ac165dbe0
Juli-Kumari <72196282+Juli-Kumari@users.noreply.github.com>
2021-04-14 18:03:03 +0530

    create banana

---
+++ app.js
@@ -0,0 +1,16 @@
+// console.log("script is working")
+// var username=prompt("Give me username"); // take input from user
+// // alert("this script works!" + username)  // it  shows buffering type things. 
+
+// // process
+// var welcomeMessage="This script work!" +username
+
+// //output
+// alert(welcomeMessage)
+
+var btnTranslate=document.querySelector("#btn-translate")
+// console.log(btnTranslate)
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!")
+
+})
